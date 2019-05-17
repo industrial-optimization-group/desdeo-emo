711a1bc1022c46f9d2c9e66385ec1dd6699b64c2
delamorte <rissanen.njs@gmail.com>
2019-05-17 15:46:57 +0300

    Changed parameter names

--- pyrvea/Examples/evonn_example.py
+++ pyrvea/Examples/evonn_example.py
@@ -1,10 +1,12 @@
 from pyrvea.Problem.evonn_problem import EvoNNProblem
+
 import numpy as np
 
 input_nodes = 4
 hidden_nodes = 6
-input_data = np.random.uniform(0, 1, size=(hidden_nodes, input_nodes))
+training_data = np.random.uniform(0, 1, size=(hidden_nodes, input_nodes))
 
-prob = EvoNNProblem(data_input=input_data, num_input_nodes=4, num_hidden_nodes=6)
+prob = EvoNNProblem(training_data_input=training_data, num_input_nodes=4, num_hidden_nodes=6)
 
 obj_func = prob.objectives()
+
