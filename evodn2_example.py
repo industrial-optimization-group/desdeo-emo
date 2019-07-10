fix: predator max moves calculation error calculating predator moves at the beginning of new gen instead of at the beginning of their turn led the population sizes growing too large (predators got too few moves)

evodn2_example.py
@@ -53,15 +53,15 @@ import plotly.graph_objs as go
 # model_evonn.plot(y, training_data_output)
 #
 #
-# test_prob = EvoNNTestProblem("Himmelblau")
+# test_prob = EvoNNTestProblem("Matyas")
 # training_data_input, training_data_output = test_prob.create_training_data(
 #     samples=250, method="random", seed=31
 # )
 #
 # model_evonn = EvoNNModel()
 # model_evonn.set_params(
-#         name="EvoNN_" + test_prob.name,
-#         algorithm=RVEA,
+#         name=test_prob.name,
+#         algorithm=PPGA,
 #         num_nodes=20,
 #         pop_size=500,
 #         prob_omit=0.2,
@@ -77,7 +77,7 @@ import plotly.graph_objs as go
 # model_evonn.fit(training_data_input, training_data_output)
 # y = model_evonn.predict(training_data_input)
 # model_evonn.plot(y, training_data_output)
-
+#
 # model_evodn2 = EvoDN2Model(name="EvoDN2_" + test_prob.name)
 # model_evodn2.set_params(
 #         pop_size=500,
@@ -214,42 +214,51 @@ import plotly.graph_objs as go
 # model_evonn.plot(y, training_data_output)
 #
 #
-# test_prob = EvoNNTestProblem("Goldstein-Price")
-# training_data_input, training_data_output = test_prob.create_training_data(
-#     samples=250, method="random", seed=31
-# )
-# model_evodn2 = EvoDN2Model(name="EvoDN2_" + test_prob.name)
-# model_evodn2.set_params(
-#         pop_size=500,
-#         subnet_struct=(6, 10),
-#         num_nodes=10,
-#         prob_omit=0.2,
-#         activation_func="sigmoid",
-#         opt_func="llsq",
-#         loss_func="rmse",
-#         selection="min_error",
-#         logging=True,
-#         plotting=True)
-#
-# model_evodn2.fit(training_data_input, training_data_output)
-# y = model_evodn2.predict(training_data_input)
-# model_evodn2.plot(y, training_data_output)
-#
-# model_evonn = EvoNNModel(name="EvoNN_" + test_prob.name)
-# model_evonn.set_params(
-#         num_nodes=20,
-#         pop_size=500,
-#         prob_omit=0.2,
-#         activation_func="sigmoid",
-#         opt_func="llsq",
-#         loss_func="rmse",
-#         selection="akaike_corrected",
-#         logging=True,
-#         plotting=True)
-#
-# model_evonn.fit(training_data_input, training_data_output)
-# y = model_evonn.predict(training_data_input)
-# model_evonn.plot(y, training_data_output)
+test_prob = EvoNNTestProblem("Goldstein-Price")
+training_data_input, training_data_output = test_prob.create_training_data(
+    samples=250, method="random", seed=31
+)
+model_evodn2 = EvoDN2Model(name="EvoDN2_" + test_prob.name)
+model_evodn2.set_params(
+        name=test_prob.name,
+        algorithm=PPGA,
+        pop_size=500,
+        num_subnets=10,
+        max_layers=10,
+        max_nodes=10,
+        prob_omit=0.2,
+        activation_func="sigmoid",
+        opt_func="llsq",
+        loss_func="rmse",
+        selection="min_error",
+        iterations=10,
+        generations_per_iteration=15,
+        logging=True,
+        plotting=True)
+
+model_evodn2.fit(training_data_input, training_data_output)
+y = model_evodn2.predict(training_data_input)
+model_evodn2.plot(y, training_data_output)
+
+model_evonn = EvoNNModel(name="EvoNN_" + test_prob.name)
+model_evonn.set_params(
+        name=test_prob.name,
+        algorithm=PPGA,
+        num_nodes=20,
+        pop_size=500,
+        prob_omit=0.2,
+        activation_func="sigmoid",
+        opt_func="llsq",
+        loss_func="rmse",
+        selection="min_error",
+        iterations=10,
+        generations_per_iteration=10,
+        logging=True,
+        plotting=True)
+
+model_evonn.fit(training_data_input, training_data_output)
+y = model_evonn.predict(training_data_input)
+model_evonn.plot(y, training_data_output)
 #
 #
 # test_prob = EvoNNTestProblem("LeviN13")
@@ -330,12 +339,12 @@ import plotly.graph_objs as go
 from deap import benchmarks
 
 
-# test_prob = EvoNNTestProblem("SchafferN1", num_of_variables=1)
+# test_prob = EvoNNTestProblem("Fonseca", num_of_variables=2)
 # training_data_input, training_data_output = test_prob.create_training_data(
 #     samples=250, method="random", seed=30
 # )
-#
-# # # ZDT 1 & 2
+
+# # ZDT 1 & 2
 
 test_prob = testProblem(
     name="ZDT1",
@@ -345,11 +354,12 @@ test_prob = testProblem(
     upper_limits=1,
     lower_limits=0,
 )
-np.random.seed(31)
+np.random.seed(30)
 training_data_input = np.random.rand(250, 30)
 training_data_output = np.asarray(
     [test_prob.objectives(x) for x in training_data_input]
 )
+
 data = np.hstack((training_data_input, training_data_output))
 f1_training_data_output = training_data_output[:, 0]
 f2_training_data_output = training_data_output[:, 1]
@@ -363,25 +373,58 @@ dataset.columns = x + y
 problem = DataProblem(data=dataset, x=x, y=y)
 problem.train_test_split()
 
-# problem.train(
-#     model_type="EvoDN2",
-#     algorithm=PPGA,
-#     pop_size=500,
-#     iterations=10,
-#     generations_per_iteration=10,
-#     recombination_type="evodn2_xover_mut_gaussian",
-# )
-
 problem.train(
-    model_type="EvoNN",
+    model_type="EvoDN2",
     algorithm=PPGA,
+    pop_size=500,
     iterations=10,
     generations_per_iteration=10,
-    crossover_type="evonn_xover",
-    mutation_type="2d_gaussian"
+    recombination_type="evodn2_xover_mut_gaussian",
 )
 
+# problem.train(
+#     model_type="EvoNN",
+#     algorithm=PPGA,
+#     iterations=10,
+#     generations_per_iteration=10,
+#     num_nodes=30,
+#     crossover_type="evonn_xover",
+#     mutation_type="2d_gaussian",
+#     recombination_type="evonn_xover_mut_gaussian"
+# )
+
+# problem.train(
+#     model_type="MLP",
+#     algorithm=RVEA,
+#     iterations=10,
+#     generations_per_iteration=10,
+#     recombination_type="evonn_xover_mut_gaussian"
+# )
+
 # problem.train(model_type="MLP")
+# mlp_reg_y_pred = problem.models["f1"][0].predict(training_data_input)
+#
+# trace0 = go.Scatter(x=mlp_reg_y_pred, y=training_data_output[:, 0], mode="markers")
+# trace1 = go.Scatter(x=training_data_output[:, 0], y=training_data_output[:, 0])
+# data = [trace0, trace1]
+# plotly.offline.plot(
+#         data,
+#         filename="MLP Regressor " + test_prob.name
+#                  + ".html",
+#         auto_open=True,
+# )
+#
+# mlp_reg_y_pred2 = problem.models["f2"][0].predict(training_data_input)
+#
+# trace0 = go.Scatter(x=mlp_reg_y_pred2, y=training_data_output[:, 1], mode="markers")
+# trace1 = go.Scatter(x=training_data_output[:, 1], y=training_data_output[:, 1])
+# data = [trace0, trace1]
+# plotly.offline.plot(
+#         data,
+#         filename="MLP Regressor f2" + test_prob.name
+#                  + ".html",
+#         auto_open=True,
+# )
 
 
 y = problem.models["f1"][0].predict(training_data_input)
@@ -419,8 +462,8 @@ pop.evolve(
 
 pop2.evolve(RVEA, iterations=10, generations_per_iteration=100)
 #
-pop.plot_pareto("evonn" + test_prob.name)
-pop2.plot_pareto("rvea" + test_prob.name)
+pop.plot_pareto(problem.models["f1"][0].__class__.__name__ + "_ppga" + test_prob.name)
+pop2.plot_pareto(problem.models["f1"][0].__class__.__name__ + "_rvea" + test_prob.name)
 
 # f1_all = pop.objectives[:, 0]
 # f2_all = pop.objectives[:, 1]

pyrvea/Problem/evonn_problem.py
@@ -55,7 +55,7 @@ class EvoNN(baseProblem):
         y_train=None,
         num_of_objectives=2,
         params=None,
-        num_samples=None
+        num_samples=None,
     ):
         super().__init__()
 
@@ -231,7 +231,9 @@ class EvoNN(baseProblem):
                 mode="markers+lines",
             )
             data = [trace0, trace1]
-            layout = go.Layout(xaxis=dict(title="training error"), yaxis=dict(title="complexity"))
+            layout = go.Layout(
+                xaxis=dict(title="training error"), yaxis=dict(title="complexity")
+            )
             plotly.offline.plot(
                 {"data": data, "layout": layout},
                 filename=self.name + "_training_models_" + "pareto" + ".html",
@@ -240,7 +242,9 @@ class EvoNN(baseProblem):
 
             model_idx = None
             while model_idx not in pop.objectives:
-                usr_input = input("Please input the number of the model of your preference: ")
+                usr_input = input(
+                    "Please input the number of the model of your preference: "
+                )
                 try:
                     model_idx = int(usr_input)
                 except ValueError:
@@ -315,8 +319,8 @@ class EvoNNModel(EvoNN):
         opt_func="llsq",
         loss_func="rmse",
         selection="akaike_corrected",
-        recombination_type=None,
-        crossover_type="EvoNN_xover",
+        recombination_type="evonn_xover_mut_gaussian",
+        crossover_type="evonn_xover",
         mutation_type="2d_gaussian",
         iterations=10,
         generations_per_iteration=10,
@@ -461,9 +465,12 @@ class EvoNNModel(EvoNN):
             The prediction of the model.
 
         """
-        out = np.dot(decision_variables, self.non_linear_layer[1:, :]) + self.non_linear_layer[0]
+        out = (
+            np.dot(decision_variables, self.non_linear_layer[1:, :])
+            + self.non_linear_layer[0]
+        )
 
-        non_linear_layer = self.activate(self.params["activation_func"], out) # rename
+        non_linear_layer = self.activate(self.params["activation_func"], out)  # rename
 
         y = np.dot(non_linear_layer, self.linear_layer)
 
@@ -485,7 +492,8 @@ class EvoNNModel(EvoNN):
         data = [trace0, trace1]
         plotly.offline.plot(
             data,
-            filename=self.__class__.__name__
+            filename=self.params['algorithm'].__name__
+            + self.__class__.__name__
             + name
             + "_var"
             + str(self.num_of_variables)
@@ -505,7 +513,9 @@ class EvoNNModel(EvoNN):
 
         # Save params to log file
         log_file = open(
-            self.name
+            self.params["algorithm"].__name__
+            + self.__class__.__name__
+            + self.name
             + "_var"
             + str(self.num_of_variables)
             + "_nodes"

pyrvea/Problem/test_functions.py
@@ -56,9 +56,9 @@ class EvoNNTestProblem(baseProblem):
             "min-ex_f1": (0, 1),
             "min-ex_f2": (0, 5),
             "Coello_ex1": (0, 1),
-            "fonseca": (-4, 4),
+            "Fonseca": (-4, 4),
             "Kursawe": (-5, 5),
-            "SchafferN1": (-100, 100)
+            "SchafferN1": (-100, 100),
         }
 
         if self.name in self.test_f_limits.keys():
@@ -167,7 +167,7 @@ class EvoNNTestProblem(baseProblem):
             # McCormick function, -1.5 <= x <= 4, -3 <= y <= 4
             x = np.asarray(decision_variables[0])
             y = np.asarray(decision_variables[1])
-            self.obj_func = np.sin(x + y) + (x - y)**2 - 1.5 * x + 2.5 * y + 1
+            self.obj_func = np.sin(x + y) + (x - y) ** 2 - 1.5 * x + 2.5 * y + 1
 
         elif self.name == "min-ex":
             x1 = decision_variables[0]
@@ -181,7 +181,11 @@ class EvoNNTestProblem(baseProblem):
             a = 2
             q = 4
             f1 = x
-            f2 = (1 + 10 * y) * (1 - (x/(1+10*y))*a - x/(1+10*y)*np.sin(2 * np.pi * q * x))
+            f2 = (1 + 10 * y) * (
+                1
+                - (x / (1 + 10 * y)) * a
+                - x / (1 + 10 * y) * np.sin(2 * np.pi * q * x)
+            )
 
             self.obj_func = [f1, f2]
 
@@ -190,14 +194,20 @@ class EvoNNTestProblem(baseProblem):
             x1 = np.asarray(decision_variables[0])
             x2 = np.asarray(decision_variables[1])
             x3 = np.asarray(decision_variables[1])
-            f1 = -10 * np.exp(-0.2 * np.sqrt(x1 ** 2 + x2 ** 2)) \
-                 - 10 * np.exp(-0.2 * np.sqrt(x2 ** 2 + x3 ** 2))
-            f2 = abs(x1) ** 0.8 + 5. * np.sin(x1 ** 3) \
-                 + abs(x2) ** 0.8 + 5. * np.sin(x2 ** 3) \
-                 + abs(x3) ** 0.8 + 5. * np.sin(x3 ** 3)
+            f1 = -10 * np.exp(-0.2 * np.sqrt(x1 ** 2 + x2 ** 2)) - 10 * np.exp(
+                -0.2 * np.sqrt(x2 ** 2 + x3 ** 2)
+            )
+            f2 = (
+                abs(x1) ** 0.8
+                + 5.0 * np.sin(x1 ** 3)
+                + abs(x2) ** 0.8
+                + 5.0 * np.sin(x2 ** 3)
+                + abs(x3) ** 0.8
+                + 5.0 * np.sin(x3 ** 3)
+            )
             return [f1, f2]
 
-        elif self.name == "fonseca":
+        elif self.name == "Fonseca":
             x1 = np.asarray(decision_variables[0])
             x2 = np.asarray(decision_variables[1])
             f1 = 1 - np.exp(-((x1 - 1 / np.sqrt(1)) ** 2 + (x2 - 1 / np.sqrt(2)) ** 2))
@@ -208,8 +218,8 @@ class EvoNNTestProblem(baseProblem):
         elif self.name == "SchafferN1":
             x = np.asarray(decision_variables[0])
 
-            f1 = x**2
-            f2 = (x - 2)**2
+            f1 = x ** 2
+            f2 = (x - 2) ** 2
 
             self.obj_func = [f1, f2]
 
@@ -234,14 +244,16 @@ class EvoNNTestProblem(baseProblem):
         if method == "random":
 
             training_data_input = np.random.uniform(
-                self.lower_limits,
-                self.upper_limits,
-                (samples, self.num_of_variables),
+                self.lower_limits, self.upper_limits, (samples, self.num_of_variables)
             )
 
             if self.name == "McCormick":
-                training_data_input[:, 0] = minmax_scale(training_data_input[:, 0], feature_range=(-1.5, 4))
-                training_data_input[:, 1] = minmax_scale(training_data_input[:, 1], feature_range=(-3, 4))
+                training_data_input[:, 0] = minmax_scale(
+                    training_data_input[:, 0], feature_range=(-1.5, 4)
+                )
+                training_data_input[:, 1] = minmax_scale(
+                    training_data_input[:, 1], feature_range=(-3, 4)
+                )
 
         elif method == "lhs":
 

pyrvea/Recombination/evodn2_xover_mut_gaussian.py
@@ -34,15 +34,30 @@ def mate(
         Standard deviation
     """
 
+    try:
+        prob_crossover = params["prob_crossover"]
+        prob_mutation = params["prob_mutation"]
+        std_dev = params["std_dev"]
+    except KeyError:
+        prob_crossover = 0.8
+        prob_mutation = 0.3
+        std_dev = (4 / 3) * (
+            1
+            - params["current_total_gen_count"] / params["total_generations"]
+        )
+        if std_dev < 0:
+            std_dev = 0
+
+    if mating_pop is None:
+        mating_pop = []
+        for i in range(len(individuals)):
+            mating_pop.append([i, np.random.randint(len(individuals))])
+
     offspring = []
 
     for mates in mating_pop:
 
         offspring1, offspring2 = deepcopy(individuals[mates[0]]), deepcopy(individuals[mates[1]])
-
-        prob_crossover = params["prob_crossover"]
-        prob_mutation = params["prob_mutation"]
-        std_dev = params["std_dev"]
 
         for subnet in range(len(offspring1)):
 