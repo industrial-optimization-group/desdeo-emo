cff95c99ba5e0169890fadd1e0c60b991effde6e
delamorte <rissanen.njs@gmail.com>
2019-08-01 14:01:44 +0300

    fixed probability calculation

--- pyrvea/Recombination/biogp_xover.py
+++ pyrvea/Recombination/biogp_xover.py
@@ -8,6 +8,7 @@ def mate(mating_pop, individuals, params):
 
     prob_standard = 0.5
     prob_height_fair = params["prob_crossover"] - prob_standard
+    r = np.random.rand()
 
     if mating_pop is None:
         mating_pop = []
@@ -27,7 +28,7 @@ def mate(mating_pop, individuals, params):
         offspring2 = deepcopy(individuals[mates[1]])
 
         # Height-fair xover
-        if np.random.rand() <= prob_height_fair:
+        if r <= prob_height_fair:
             depth = min(offspring1.max_depth, offspring2.max_depth)
             rand_node1 = choice(offspring1.nodes_at_depth[depth])
             rand_node2 = choice(offspring2.nodes_at_depth[depth])
@@ -39,7 +40,7 @@ def mate(mating_pop, individuals, params):
             rand_node2.roots = tmp_roots
 
         # Standard xover
-        elif np.random.rand() <= prob_height_fair + prob_standard:
+        elif r <= prob_height_fair + prob_standard:
             rand_node1 = choice(offspring1.nodes[1:])  # Exclude linear node
             rand_node2 = choice(offspring2.nodes[1:])
             tmp_value = rand_node1.value
