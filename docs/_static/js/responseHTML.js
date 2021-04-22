bd8cdb78e36b76c3d273c18a920ad0342bb9b9e1
Giomara Lárraga Maldonado <glarragw@it-adam-2012.it.jyu.fi>
2021-04-23 01:24:49 +0300

    Change layout

---
+++ docs/_static/js/responseHTML.js
@@ -0,0 +1,104 @@
+/**
+	responseHTML
+	(c) 2007-2008 xul.fr		
+	Licence Mozilla 1.1
+*/	
+
+
+/**
+	Searches for body, extracts and return the content
+	New version contributed by users
+*/
+
+
+function getBody(content) 
+{
+   test = content.toLowerCase();    // to eliminate case sensitivity
+   var x = test.indexOf("<body");
+   if(x == -1) return "";
+
+   x = test.indexOf(">", x);
+   if(x == -1) return "";
+
+   var y = test.lastIndexOf("</body>");
+   if(y == -1) y = test.lastIndexOf("</html>");
+   if(y == -1) y = content.length;    // If no HTML then just grab everything till end
+
+   return content.slice(x + 1, y);   
+} 
+
+/**
+	Loads a HTML page
+	Put the content of the body tag into the current page.
+	Arguments:
+		url of the other HTML page to load
+		id of the tag that has to hold the content
+*/		
+
+function loadHTML(url, fun, storage, param)
+{
+	var xhr = createXHR();
+	xhr.onreadystatechange=function()
+	{ 
+		if(xhr.readyState == 4)
+		{
+			//if(xhr.status == 200)
+			{
+				storage.innerHTML = getBody(xhr.responseText);
+				fun(storage, param);
+			}
+		} 
+	}; 
+
+	xhr.open("GET", url , true);
+	xhr.send(null); 
+
+} 
+
+	/**
+		Callback
+		Assign directly a tag
+	*/		
+
+
+	function processHTML(temp, target)
+	{
+		target.innerHTML = temp.innerHTML;
+	}
+
+	function loadWholePage(url)
+	{
+		var y = document.getElementById("storage");
+		var x = document.getElementById("displayed");
+		loadHTML(url, processHTML, x, y);
+	}	
+
+
+	/**
+		Create responseHTML
+		for acces by DOM's methods
+	*/	
+	
+	function processByDOM(responseHTML, target)
+	{
+		target.innerHTML = "Extracted by id:<br />";
+
+		// does not work with Chrome/Safari
+		//var message = responseHTML.getElementsByTagName("div").namedItem("two").innerHTML;
+		var message = responseHTML.getElementsByTagName("div").item(1).innerHTML;
+		
+		target.innerHTML += message;
+
+		target.innerHTML += "<br />Extracted by name:<br />";
+		
+		message = responseHTML.getElementsByTagName("form").item(0);
+		target.innerHTML += message.dyn.value;
+	}
+	
+	function accessByDOM(url)
+	{
+		//var responseHTML = document.createElement("body");	// Bad for opera
+		var responseHTML = document.getElementById("storage");
+		var y = document.getElementById("displayed");
+		loadHTML(url, processByDOM, responseHTML, y);
+	}	
--- docs/conf.py
+++ docs/conf.py
@@ -73,5 +73,6 @@ html_sourcelink_suffix = ""
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_logo = "_static/desdeo_logo.png"
 
 master_doc = "index"
