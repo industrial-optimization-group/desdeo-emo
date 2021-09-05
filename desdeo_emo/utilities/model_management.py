6c568803584b2e1e2cfe9251d02e9347e714da0f
Pouya Aghaei Pour <39522102+ppouyaa@users.noreply.github.com>
2021-09-05 23:05:18 +0300

    Add files via upload
    IK-RVEA

--- desdeo_emo/utilities/model_management.py
+++ desdeo_emo/utilities/model_management.py
@@ -1,5 +1,6 @@
 from numpy.core.numeric import indices
 from numpy.lib.arraysetops import unique
+from pandas.core.frame import DataFrame
 from desdeo_tools.scalarization.ASF import SimpleASF, ReferencePointASF
 from numba import njit
 import numpy as np
@@ -8,6 +9,8 @@ import copy
 from desdeo_problem.surrogatemodels.SurrogateModels import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import DotProduct,\
     WhiteKernel, RBF, Matern, ConstantKernel
+from desdeo_problem import MOProblem
+
 
 
 #TODO: add @njit function here
@@ -23,27 +26,20 @@ def remove_duplicate(
     Returns: 
     indicies (np.ndarray): the indicies of solutions that are NOT already in the archive.
     """
-    indicies = None
-    archive = archive_x.to_numpy()
-    tmp = X
-    for i in archive:
-        for k in range(len(X)):
-            for j in range(len(i)):
-                tmp[k,j] = X[k,j] - i[j]
-        tmp = np.round(tmp, 2)
-        if indicies is None:
-            indicies = np.where(~tmp.any(axis=1))[0]
-        else:
-            tmp = np.where(~tmp.any(axis=1))[0]
-            if tmp.size > 0:
-                indicies = np.hstack((indicies.squeeze(),tmp))
-        if indicies.size == 0:
-            indicies = None
-    if indicies is None:
-        return None
-    else:
 
-        return indicies
+    all_variables = np.vstack((archive_x,X))
+    all_variables_indicies = np.arange(len(all_variables))
+
+    e,unique_variables_indicies = np.unique(all_variables, return_index=True, axis=0)
+
+    repeated_all_variables = np.delete(all_variables_indicies, unique_variables_indicies)
+    repeated_in_X = repeated_all_variables - len(archive_x)
+    X_indicies = np.arange(len(X))
+    X_uniqe_indicies = np.delete(X_indicies, repeated_in_X)
+
+        
+    return X_uniqe_indicies
+
 
 
 def ikrvea_mm(
@@ -51,7 +47,7 @@ def ikrvea_mm(
     individuals: np.ndarray,
     objectives: np.ndarray,
     uncertainity: np.ndarray,
-    problem,
+    problem: MOProblem,
     u: int) -> float:
     """ Selects the solutions that need to be reevaluated with the original functions.
     This model management is based on the following papaer: 
@@ -70,33 +66,26 @@ def ikrvea_mm(
 
     Returns:
         float: the new problem object that has an updated archive.
-    """
-
-    archive = problem.archive.to_numpy()
-    decision_variables = copy.deepcopy(individuals)
-    nd = remove_duplicate(decision_variables, problem.archive.drop(
-            problem.objective_names, axis=1)) #removing duplicate solutions
-    if nd is not None:
+    """     
+    
+    nd = remove_duplicate(individuals, problem.archive.drop(
+            problem.objective_names, axis=1).to_numpy()) #removing duplicate solutions
+    if len(nd) == 0:
+        return problem
+    else:
         non_duplicate_dv = individuals[nd]
         non_duplicate_obj = objectives[nd]
         non_duplicate_unc = uncertainity[nd]
-    else:
-        non_duplicate_dv = individuals
-        non_duplicate_obj = objectives
-        non_duplicate_unc = uncertainity
-
+        
     # Selecting solutions with lowest ASF values
     asf_solutions = SimpleASF([1]*problem.n_of_objectives).__call__(non_duplicate_obj, reference_point)
     idx = np.argpartition(asf_solutions, 2*u)
     asf_unc = np.max(non_duplicate_unc [idx[0:2*u]], axis= 1)
     # index of solutions with lowest Uncertainty
     lowest_unc_index = np.argpartition(asf_unc, u)[0:u]
-    names = np.hstack((problem.variable_names,problem.objective_names))
-    reevaluated_objs = problem.evaluate(non_duplicate_dv[lowest_unc_index], use_surrogate=False)[0]
-    new_results = np.hstack((non_duplicate_dv[lowest_unc_index], reevaluated_objs))
-    archive = np.vstack((archive, new_results))
-    new_archive = pd.DataFrame(archive, columns=names)
-    problem.archive = new_archive #updating the archive
+    # evaluating the solutions in asf_unc with lowest uncertainty. The archive will get update in problem.evaluate()
+    problem.evaluate(non_duplicate_dv[lowest_unc_index], use_surrogate=False)[0] 
+    
     problem.train(models=GaussianProcessRegressor,\
          model_parameters={'kernel': Matern(nu=1.5)}) 
 
