e91bc83ced3bb57b18028da62624fd487f276eab
Severi_Nattila <severi.nattila@gmail.com>
2023-06-05 10:11:10 +0300

    test linter

--- tests/test_IBEA.py
+++ tests/test_IBEA.py
@@ -1,8 +1,9 @@
 import pytest
 import numpy as np
 #linter should remove this: 
-from desdeo_emo.population.Population import Population
+import pandas as pd
 
+from desdeo_emo.population.Population import Population
 from desdeo_emo.EAs.IBEA import IBEA
 from desdeo_problem.problem import Variable, ScalarObjective
 from desdeo_problem import variable_builder, ScalarObjective, MOProblem
